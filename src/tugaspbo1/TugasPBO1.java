62969034052ead0a0c345db2a206ba02d1e8302e
kekerofficial <43506956+kekerofficial@users.noreply.github.com>
2018-09-23 11:43:39 +0700

    Add files via upload

---
+++ src/tugaspbo1/TugasPBO1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspbo1;
+
+/**
+ *
+ * @author 
+ * NAMA : Irfan Faisal Hutama Tanjung.
+ * KELAS : PBO2
+ * NIM : 10117060
+ * Deskripsi Program : Program ini berisi menampilkan data ke layar
+ * 
+ */
+public class TugasPBO1 {
+
+    /**
+     * @param args the command line argumenuuoots
+     */
+    public static void main(String[] args) {
+        // biodata
+        System.out.println("Biodata Rockstar" + "\n" + "--------------------");
+
+        System.out.println("Nama\t\t : Irfan Faisal Hutama Tanjung");
+        System.out.println("Alamat\t\t : Jln. Tubagus Ismail Dalam no. 40 D");
+        System.out.println("Tempat Lahir\t : Medan");
+        System.out.println("Tanggal Lahir\t : 20 MEI 1999");
+        System.out.println("---------------------------------------------");
+        System.out.println("Hobi\t\t : Makan");
+        System.out.println("MAKES\t\t : Nasi Goreng, Sayur bayam");
+        System.out.println("MIKES\t\t : Kopi");
+
+    }
+
+}
